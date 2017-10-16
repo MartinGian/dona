f08fe158bc4d62e96be81b04a3543d0abea00365
Martin Giannechini <martingiannechini@gmail.com>
2017-10-16 19:13:28 -0300

    Added CachedImageCell to avoid show old Sources on recycled cells

---
+++ Portable/Common/CachedImageCell.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using dona.Forms.Model;
+using FFImageLoading.Forms;
+using Xamarin.Forms;
+
+namespace dona.Forms.Common
+{
+    public class CachedImageCell : ViewCell
+    {
+        private CachedImage _cachedImage;
+
+        protected override void OnBindingContextChanged()
+        {
+            if (_cachedImage == null)
+                _cachedImage = (CachedImage)View.LogicalChildren.FirstOrDefault(x => x is CachedImage);
+
+            if (_cachedImage == null) return;
+
+            _cachedImage.Source = null; // prevent showing old images occasionally
+
+            var item = BindingContext as Institution;
+            if (item == null)
+                return;
+
+            _cachedImage.Source = item.CoverSource;
+
+            base.OnBindingContextChanged();
+        }
+    }
+}
